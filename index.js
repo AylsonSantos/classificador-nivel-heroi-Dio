be818e9a4a0cf0a3d9729fcecd417111c923d27b
AylsonSantos <aylson.a@gmail.com>
2024-09-24 18:35:53 -0300

    Estruturando entrada de dados

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// projeto Dio
+// projeto Dio Classificação de Heroi
+// entrada de dados teste
+
+let personagens = ["kiry:1500","mili:800"]
+let classificacion = []
+
+personagens.forEach( xp => {
+    let [nome, nivel] = xp.split(':');
+    nivel = parseInt(nivel);
+
+    // Estrutura de validação de xp
+
+    if(nivel < 1000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Ferro`)
+    } else if(nivel > 1001 && nivel < 2000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Bronze`)
+    }
+})
+
+console.log(classificacion)
