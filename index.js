65977a5185b4073a4477f0f4f1a37f04dd722b05
AylsonSantos <aylson.a@gmail.com>
2024-09-25 09:52:17 -0300

    Finalização e teste

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // projeto Dio Classificação de Heroi
 // entrada de dados teste
 
-let personagens = ["kiry:1500","mili:800"]
+let personagens = ["kiry:1500","mili:800","Rafa:13500"]
 let classificacion = []
 
 personagens.forEach( xp => {
@@ -14,6 +14,18 @@ personagens.forEach( xp => {
         classificacion.push(`O Herói de nome ${nome} está no nível de Ferro`)
     } else if(nivel > 1001 && nivel < 2000){
         classificacion.push(`O Herói de nome ${nome} está no nível de Bronze`)
+    }else if(nivel > 2001 && nivel < 4000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Prata`)
+    }else if(nivel > 4001 && nivel < 6000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Ouro`)
+    }else if(nivel > 6001 && nivel < 8000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Platina`)
+    }else if(nivel > 8001 && nivel < 10000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Ascendente`)
+    }else if(nivel > 10001 && nivel < 12000){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Imortal`)
+    }else if(nivel >= 12001){
+        classificacion.push(`O Herói de nome ${nome} está no nível de Radiante`)
     }
 })
 
